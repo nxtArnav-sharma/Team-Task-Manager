f691c980555c68c6e8a5eda7567eaf5e6e4962c0
Arnav <sharma.arnavsh6@gmail.com>
2026-05-14 23:16:53 +0530

    debug: add request logging

--- src/app.js
+++ src/app.js
@@ -12,6 +12,12 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
+// Logger
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
 // Serve landing page on root
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../public/pages/landing.html')));
 
